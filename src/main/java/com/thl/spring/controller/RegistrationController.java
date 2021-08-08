85570785f133d415ad5f999395dceae63082e287
thloykot <ihor.slob@gmail.com>
2021-08-09 02:48:32 +0300

    NoopEncoder replased
    NoopEncoder replased v2
    
    NoopEncoder replased v2
    
    NoopEncoder replased v2

--- src/main/java/com/thl/spring/controller/RegistrationController.java
+++ src/main/java/com/thl/spring/controller/RegistrationController.java
@@ -3,7 +3,9 @@ package com.thl.spring.controller;
 import com.thl.spring.dao.UserDao;
 import com.thl.spring.model.Role;
 import com.thl.spring.model.User;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,11 +13,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import java.util.Collections;
 import java.util.Optional;
 
+@RequiredArgsConstructor
 @Controller
 public class RegistrationController {
 
-    @Autowired
-    UserDao userDao;
+    private final PasswordEncoder passwordEncoder;
+    private final UserDao userDao;
 
     @GetMapping("/registration")
     public String registration() {
@@ -31,6 +34,7 @@ public class RegistrationController {
         }
         user.setActive(true);
         user.setUserRole(Collections.singleton(Role.USER));
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         userDao.save(user);
         return "redirect:/login";
     }
