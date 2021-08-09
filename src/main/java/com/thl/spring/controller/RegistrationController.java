3bb4c1f33631f6b041d5cf1ad04142b50061d164
thloykot <ihor.slob@gmail.com>
2021-08-09 09:23:01 +0300

    checkUserExist

--- src/main/java/com/thl/spring/controller/RegistrationController.java
+++ src/main/java/com/thl/spring/controller/RegistrationController.java
@@ -4,10 +4,11 @@ import com.thl.spring.dao.UserDao;
 import com.thl.spring.model.Role;
 import com.thl.spring.model.User;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.Collections;
@@ -26,10 +27,11 @@ public class RegistrationController {
     }
 
     @PostMapping("/registration")
-    public String addUser(User user) {
+    public String addUser(@ModelAttribute User user, Model model) {
         Optional<User> userFromBD = userDao.findByUsername(user.getUsername());
 
         if (userFromBD.isPresent()) {
+            model.addAttribute("existedUsername",model);
             return "registration";
         }
         user.setActive(true);
